ccd83314f6dbf6ac1a9169739ebf29a6b0810307
Astrolonium <115942738+astrolonium@users.noreply.github.com>
2022-11-27 08:54:29 -0800

    Add files via upload

---
+++ gem.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+root = Tk()
+
+ #creating aILabel Widget
+myLabel1 = Label(root, text="Hellow world! ")
+myLabel2 = Label(root, text="Hellow world! is ok ")
+myLabel3 = Label(root, text="Teh wolrd is fine ")
+myLabel4 = Label(root, text="thw world can got to hell")
+myLabel5 = Label(root, text="oramge is orange not orange ")
+myLabel6 = Label(root, text="u can go home shore ")
+myLabel7 = Label(root, text="the world is round bro ")
+myLabel8 = Label(root, text="apple is red because it is red ")
+myLabel9 = Label(root, text="mango is dango ploo")
+myLabel10 = Label(root, text="say polo  punk now ")
+myLabel20 = Label(root, text="its ok i got it")
+myLabel21 = Label(root, text="this is a rpow ")
+myLabel22 = Label(root, text="Hellow world! is ok ")
+myLabel23 = Label(root, text="Hellow world! is ok ")
+myLabel24 = Label(root, text="Hellow world! is ok ")
+myLabel25 = Label(root, text="Hellow world! is ok ")
+myLabel26 = Label(root, text="Hellow world! is ok ")
+myLabel27 = Label(root, text="Hellow world! is ok ")
+myLabel28 = Label(root, text="Hellow world! is ok ")
+myLabel29 = Label(root, text="Hellow world! is ok ")
+
+#Shoving it onto the screen
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=2)
+myLabel3.grid(row=2, column=0)
+myLabel4.grid(row=3, column=0)
+myLabel5.grid(row=4, column=0)
+myLabel6.grid(row=5, column=0)
+myLabel7.grid(row=6, column=0)
+myLabel8.grid(row=7, column=0)
+myLabel9.grid(row=8, column=0)
+myLabel10.grid(row=9, column=0)
+myLabel20.grid(row=10, column=0)
+myLabel21.grid(row=11, column=0)
+myLabel22.grid(row=12, column=0)
+myLabel23.grid(row=13, column=0)
+
+
+
+root.mainloop()
