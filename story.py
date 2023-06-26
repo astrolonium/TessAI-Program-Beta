1675f5707933f79606e3fa82e17393b7d43c4dbd
Rafhan <astrolonium@gmail.com>
2023-06-25 20:28:46 -0700

    Update story.py

--- story.py
+++ story.py
@@ -2,15 +2,15 @@ import speech_recognition as sr
 from gtts import gTTS
 import os
 
-# Initialize speech recognition engine
+
 r = sr.Recognizer()
 
-# Use the microphone as audio source
+
 with sr.Microphone() as source:
     print("Please read a story for me.")
     audio = r.listen(source)
 
-# Convert speech to text
+
 try:
     text = r.recognize_google(audio)
     print(f"You said: {text}")
@@ -19,7 +19,7 @@ except sr.UnknownValueError:
 except sr.RequestError as e:
     print(f"Could not request results from Google Speech Recognition service; {e}")
 
-# Convert text to speech and speak it out loud
+
 tts = gTTS(text)
 tts.save("output.mp3")
 os.system("start output.mp3")
