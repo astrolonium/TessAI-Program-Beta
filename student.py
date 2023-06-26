1040fe1584237160e13974df6b0404d97a71272c
Rafhan <astrolonium@gmail.com>
2023-06-25 20:29:15 -0700

    Update student.py

--- student.py
+++ student.py
@@ -1,12 +1,12 @@
 from tkinter import *
 import random
 
-# Set up the window
+
 window = Tk()
 window.title("Student Lottery")
 window.geometry("400x400")
 
-# Set up the labels and entry fields
+
 name_label = Label(window, text="Enter Student Name:")
 name_label.pack()
 name_entry = Entry(window)
@@ -22,7 +22,7 @@ grade_label.pack()
 grade_entry = Entry(window)
 grade_entry.pack()
 
-# Set up the button to add student to list
+
 student_list = []
 
 def add_student():
@@ -39,7 +39,7 @@ def add_student():
 add_button = Button(window, text="Add Student", command=add_student)
 add_button.pack()
 
-# Set up the button to select a student at random
+
 def select_student():
     if student_list:
         selected_student = random.choice(student_list)
@@ -49,10 +49,9 @@ def select_student():
 select_button = Button(window, text="Select Student", command=select_student)
 select_button.pack()
 
-# Set up the label to display cob winner
+
 cob_label = Label(window, text="")
 cob_label.pack()
 
-# Run the window
+
 window.mainloop()
-#another useless line to fake the contribution chart
