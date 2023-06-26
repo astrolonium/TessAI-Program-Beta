b2da2d638dd2fc99e83a0f8f963888847a6cde69
Rafhan <astrolonium@gmail.com>
2023-06-25 20:21:34 -0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,16 +1,14 @@
 import speech_recognition as sr
 import pyttsx3
 
-# Initialize speech recognition and text-to-speech engines
+
 r = sr.Recognizer()
 engine = pyttsx3.init()
-
-# Use the microphone as audio source
 with sr.Microphone() as source:
     print("Please say your name.")
     audio = r.listen(source)
 
-# Convert speech to text
+
 try:
     name = r.recognize_google(audio)
     print(f"Hello, {name}!")
@@ -20,4 +18,4 @@ except sr.UnknownValueError:
     print("Sorry, I could not understand what you said.")
 except sr.RequestError as e:
     print(f"Could not request results from Google Speech Recognition service; {e}")
-#fake
+
